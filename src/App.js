b100f32a74cf518cc87eae99ba2041030bdcfe0f
mezzDavies <93786981+mezzDavies@users.noreply.github.com>
2022-08-06 11:00:33 +0200

    shuffle cards function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 
+const cardImages = [
+  { src: "/img/helmet-1" },
+  { src: "/img/potion-1" },
+  { src: "/img/ring-1" },
+  { src: "/img/scroll-1" },
+  { src: "/img/shield-1" },
+  { src: "/img/sword-1" },
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => {
+        return { ...card, id: Math.random() };
+      });
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+  console.log("cards are shuffled", cards, turns);
+
   return (
     <div className="App">
       <h1>Magic match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
